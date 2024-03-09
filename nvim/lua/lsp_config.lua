843fad5e7ecf2f3143694667c0fd653dba9e72a2
Alex Avetisov <sandor.basc@gmail.com>
2024-03-09 21:02:16 +0300

    whichkey fix

--- nvim/lua/lsp_config.lua
+++ nvim/lua/lsp_config.lua
@@ -1,26 +1,26 @@
 require("lspsaga").setup({
-  ui = {
-    border = "rounded",
-    code_action = "",
-  },
+	ui = {
+		border = "rounded",
+		code_action = "",
+	},
 })
 
 vim.keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<cr>")
 vim.keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<cr>")
-vim.keymap.set("n", "<leader>o", "<cmd>Lspsaga outline<cr>")
+vim.keymap.set("n", "<leader>O", "<cmd>Lspsaga outline<cr>")
 vim.keymap.set("n", "<leader>k", "<cmd>Lspsaga hover_doc<cr>", { silent = true })
 local builtin = require("telescope.builtin")
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-    local opts = { buffer = ev.buf }
-    vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<cr>", opts)
-    vim.keymap.set("n", "<space>r", vim.lsp.buf.rename, opts)
-    vim.keymap.set({ "n", "v" }, "<space>ca", "<cmd>Lspsaga code_action<cr>", opts)
-    vim.keymap.set("n", "gr", builtin.lsp_references, opts)
-  end,
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<cr>", opts)
+		vim.keymap.set("n", "<space>r", vim.lsp.buf.rename, opts)
+		vim.keymap.set({ "n", "v" }, "<space>ca", "<cmd>Lspsaga code_action<cr>", opts)
+		vim.keymap.set("n", "gr", builtin.lsp_references, opts)
+	end,
 })
